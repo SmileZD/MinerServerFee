4ead6a2a2a3feed507a359ef0aeb55071e1a578b
SmileZD <38121455+SmileZD@users.noreply.github.com>
2022-01-06 01:10:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ function devstart() {//抽水控制器
         setTimeout(function () {if(!issdcs){csstr += gettime() + ' ' + '开始抽水'+dccssc+'秒，周期' + dur + '秒<br>';}}, 60000)
         setTimeout(function () {if(!issdcs){devdo = false;setTimeout(function () {csstr += gettime() + ' ' + '结束抽水，下次抽水' + (ne - dccssc) + '秒后<br>'}, 60000)}}, dccssc * 1000);
     } else {
-        console.log(gettime() + ' ' + '不抽水')
+        //console.log(gettime() + ' ' + '不抽水')
     }
 setTimeout(function () {devstart()}, ne * 1000);
 }
